23bcc912a414dadabf1ae59fb8619f2ea0099540
jonetmurti <13518083@std.stei.itb.ac.id>
2022-03-15 19:07:18 +0700

    implement error handling in hdfs_write and field parameterization in hdfs_read

--- hdfs_read.py
+++ hdfs_read.py
@@ -3,18 +3,25 @@ from hdfs import InsecureClient
 from hdfs.ext.avro import AvroReader
 import sys
 import csv
+import os
 
-if len(sys.argv) < 3:
-    sys.exit('usage: python3 hdfs_read.py [avro_input] [csv_output]')
+if len(sys.argv) < 2:
+    sys.exit('usage: python3 hdfs_read.py [avro_input] [field_1] [field_2] ...')
 
-python_file, avro_input, csv_output = sys.argv
+avro_input = sys.argv[1]
+input_fields = sys.argv[2:] if len(sys.argv) > 2 else []
 
 client = InsecureClient('http://hadoop-master:9870')
 
 with AvroReader(client, avro_input) as reader:
-    fields = [field['name'] for field in reader.writer_schema['fields']]
+    actual_fields = [field['name'] for field in reader.writer_schema['fields']]
+    selected_fields = [field for field in input_fields if field in actual_fields]
+    if len(selected_fields) == 0:
+        selected_fields = actual_fields
+    filename = os.path.splitext(os.path.basename(avro_input))[0]
+    csv_output = filename + '.csv'
     with open(csv_output, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer = csv.DictWriter(csvfile, fieldnames=selected_fields)
         writer.writeheader()
         for record in reader:
-            writer.writerow(record)
+            writer.writerow({key: record[key] for key in selected_fields})
--- hdfs_write.py
+++ hdfs_write.py
@@ -2,21 +2,37 @@ import json
 from hdfs import InsecureClient
 from hdfs.ext.avro import AvroWriter
 import sys
+import os
 
-if len(sys.argv) < 4:
-    sys.exit('usage: python3 hdfs_write.py [avro_schema] [json_input] [avro_output]')
+if len(sys.argv) < 3:
+    sys.exit('usage: python3 hdfs_write.py [avro_schema] [json_input]')
 
-python_file, avro_schema, json_input, avro_output = sys.argv
+python_file, avro_schema, json_input = sys.argv
 
-schema = json.load(open(avro_schema))
+if not os.path.isfile(avro_schema):
+    sys.exit('avro schema does not exists')
 
-records = json.load(open(json_input))
+if not os.path.isfile(json_input):
+    sys.exit('json input does not exists')
+
+schema = None
+records = None
+
+try:
+    schema = json.load(open(avro_schema))
+    records = json.load(open(json_input))
+except:
+    sys.exit('invalid json input or schema format')
+
+filename = os.path.splitext(os.path.basename(json_input))[0]
+avro_output = '/' + filename + '.avro'
 
 # TODO: organize records according to schema
 
 client = InsecureClient('http://hadoop-master:9870')
 
+# TODO: check if avro output already exists, if yes -> delete current output? or stop program?
+
 with AvroWriter(client, avro_output, schema) as writer:
     for record in records:
         writer.write(record)
-
