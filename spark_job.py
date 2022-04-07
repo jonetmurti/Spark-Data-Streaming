231a73e40f7d87517a06e2b381cfc5333c25858c
jonetmurti <13518083@std.stei.itb.ac.id>
2022-04-07 14:26:00 +0700

    add table empty condition in spark_job

--- spark_job.py
+++ spark_job.py
@@ -1,12 +1,15 @@
 from pyspark.sql import SparkSession
+
 import pyspark.sql.functions as sf
 
 spark = SparkSession.builder.master('spark://hadoop-master:7077') \
-                    .appName('haha') \
+                    .appName('test') \
                     .getOrCreate()
 
 df = spark.read.format('avro').load('hdfs://hadoop-master:9000/social_media.avro')
+
 df_rename = df.withColumnRenamed('created_at', 'timestamp')
+
 df_group = df_rename.groupBy(['timestamp', 'social_media']) \
              .agg(
                 sf.count('original_id').alias('stream_count'),
@@ -37,12 +40,25 @@ def map_condition(row):
     else:
         return (row[0], row[1], row[2], row[3], row[4], row[5])
 
-df_join = df_drop_id.join(df_timestamp, ['timestamp', 'social_media'], 'outer')
-rdd_map = df_join.rdd.map(map_condition)
-new_df = rdd_map.toDF(['timestamp', 'social_media', 'stream_count', 'unique_user_count', 'created_at', 'updated_at'])
-df_order = new_df.orderBy(['social_media', 'timestamp'])
-df_id = df_order.withColumn('id', sf.monotonically_increasing_id()) \
-              .select('id', 'timestamp', 'social_media', 'stream_count', 'unique_user_count', 'created_at', 'updated_at')
+if len(df_drop_id.head(1)) == 0:
+  df_order = df_timestamp.orderBy(['social_media', 'timestamp'])
 
-df_id.write.mode("overwrite").jdbc(url=host, table="social_media.summary", properties=conn_properties)
+  df_id = df_order.withColumn('id', sf.monotonically_increasing_id()) \
+                .select('id', 'timestamp', 'social_media', 'stream_count', 'unique_user_count', 'created_at', 'updated_at')
+
+  df_id.write.mode("overwrite").jdbc(url=host, table="social_media.summary", properties=conn_properties)
+else:
+  df_join = df_drop_id.join(df_timestamp, ['timestamp', 'social_media'], 'outer')
+
+  rdd_map = df_join.rdd.map(map_condition)
+
+  new_df = rdd_map.toDF(['timestamp', 'social_media', 'stream_count', 'unique_user_count', 'created_at', 'updated_at'])
+
+  df_order = new_df.orderBy(['social_media', 'timestamp'])
+
+  df_id = df_order.withColumn('id', sf.monotonically_increasing_id()) \
+                .select('id', 'timestamp', 'social_media', 'stream_count', 'unique_user_count', 'created_at', 'updated_at')
+
+  df_id.write.mode("overwrite").jdbc(url=host, table="social_media.summary", properties=conn_properties)
+
 
