b2cc85438aece99853e00e8a8b08ad0bda910c2d
jonetmurti <13518083@std.stei.itb.ac.id>
2022-04-26 21:45:14 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,35 @@ else
 	echo "$FILE already exist."
 fi
 
+KAFKA_FILE=/home/project2-bigdata/Frameworks/kafka_2.13-3.1.0.tgz
+if [ ! -f "$KAFKA_FILE" ]; then
+    echo "Downloading $KAFKA_FILE."
+	wget -P /home/project2-bigdata/Frameworks https://dlcdn.apache.org/kafka/3.1.0/kafka_2.13-3.1.0.tgz
+else 
+	echo "$KAFKA_FILE already exist."
+fi
+
 if [ ! -d "/home/project2-bigdata/Frameworks/spark-2.4.7-bin-hadoop2.7" ]; then
 	echo "Extracting $FILE."
 	tar xvf $FILE
 else
 	echo "$FILE already extracted."
+fi
+
+if [ ! -d "/home/project2-bigdata/Frameworks/kafka_2.13-3.1.0" ]; then
+	echo "Extracting $KAFKA_FILE."
+	tar xvf $KAFKA_FILE
+	echo "export KAFKA_HOME=/home/project2-bigdata/Frameworks/kafka_2.13-3.1.0" >> ~/.bashrc
+	echo "export PATH=$PATH:$KAFKA_HOME/bin" >> ~/.bashrc
+	sudo touch /etc/systemd/system/zookeeper.service
+	sudo cat ./zookeeper.service > /etc/systemd/system/zookeeper.service
+	sudo bash -c 'cat ./zookeeper.service > /etc/systemd/system/zookeeper.service'
+	sudo touch  /etc/systemd/system/kafka.service
+	sudo bash -c 'cat ./kafka.service > /etc/systemd/system/kafka.service'
+	sudo systemctl start kafka
+	sudo journalctl -u kafka
+else
+	echo "$KAFKA_FILE already extracted."
 fi
 
 if [ ! -d "./venv" ]; then
@@ -25,7 +49,7 @@ source ./venv/bin/activate
 PYTHON_VERSION=$(which python3)
 if [ $PYTHON_VERSION != "/usr/bin/python3\n" ]; then
 	pip install -r ./requirements.txt
-	rm ./venv.tar.gz
+	rm -f ./venv.tar.gz
 	venv-pack -o venv.tar.gz
 else 
 	echo "Environment not set."
--- spark_streaming.sh
+++ spark_streaming.sh
@@ -1,3 +1,7 @@
 source ./venv/bin/activate
+export SPARK_HOME=/home/project2-bigdata/Frameworks/spark-2.4.7-bin-hadoop2.7
+export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
 export PYSPARK_PYTHON=./venv/bin/python
-spark-submit --archives venv.tar.gz --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.7 --master spark://project2-bigdata:7077 /home/project2-bigdata/Projects/project-2-tbd/spark_streamig.py
+echo $SPARK_HOME
+echo $PYSPARK_PYTHON
+# spark-submit --archives venv.tar.gz --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.7 --master spark://project2-bigdata:7077 /home/project2-bigdata/Projects/project-2-tbd/spark_streamig.py
